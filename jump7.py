53c8cc57e62c1db99f52996c4ca67bf6e1a46f0f
florencelei <438694720@qq.com>
2019-08-01 15:09:24 +0800

    7game

---
+++ jump7.py
@@ -0,0 +1,13 @@
+a = 1
+while a < 101 :
+    if a%7 == 0 :    #7d???
+        pass   
+    elif a//10 == 7 :  #????7??
+        pass
+    elif a%10 == 7 :  #????7??
+        a = a+1
+        continue
+    else:
+        print(a,end=' ')
+    a = a +1
+
